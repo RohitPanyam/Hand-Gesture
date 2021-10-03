34c31fb6c959395d0cfb90ab1537ae2a00dea1e5
Rohit Panyam <panyamrohitreddy@gmail.com>
2021-10-03 10:21:58 +0530

    Basic Code

---
+++ Hand_tracking.py
@@ -0,0 +1,1 @@
+import cv2import mediapipe as mpimport timecap = cv2.VideoCapture(0)while True:    success, img = cap.read()    cv2.imshow("image", img)    if cv2.waitKey(1) & 0xFF == ord('q'):        break
